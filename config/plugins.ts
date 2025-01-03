11070dcc78f41c6cfce80b032963d9af5a3f6f2a
gabrielbaltar <201902046293@alunos.estacio.br>
2025-01-03 20:45:08 -0300

    feat: new provider config

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,12 +1,18 @@
 module.exports = ({ env }) => ({
     upload: {
       config: {
-        provider: '@strapi/provider-upload-supabase',
+        provider: '@strapi/provider-upload-aws-s3',
         providerOptions: {
-          apiUrl: env('SUPABASE_API_URL'),
-          apiKey: env('SUPABASE_API_KEY'),
-          bucket: env('SUPABASE_BUCKET'),
-          directory: env('SUPABASE_DIRECTORY'), // Subdiretório opcional
+          accessKeyId: env('SUPABASE_ACCESS_KEY'), // Chave pública de acesso (Service Role Key)
+          secretAccessKey: env('SUPABASE_SECRET_ACCESS_KEY'), // Chave secreta de acesso (Service Role Key)
+          region: env('SUPABASE_REGION'), // Região padrão do Supabase
+          params: {
+            Bucket: env('SUPABASE_BUCKET'), // Nome do bucket
+          },
+        },
+        actionOptions: {
+          upload: {},
+          delete: {},
         },
       },
     },

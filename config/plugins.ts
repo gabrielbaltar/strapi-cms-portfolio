7e7a6ed6e8f6a95054651048ad9ece8faf4bf12c
gabrielbaltar <201902046293@alunos.estacio.br>
2025-01-03 20:23:09 -0300

    feat: new plugin config

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,1 +1,14 @@
-export default () => ({});
+module.exports = ({ env }) => ({
+    upload: {
+      config: {
+        provider: '@strapi/provider-upload-supabase',
+        providerOptions: {
+          apiUrl: env('SUPABASE_API_URL'),
+          apiKey: env('SUPABASE_API_KEY'),
+          bucket: env('SUPABASE_BUCKET'),
+          directory: env('SUPABASE_DIRECTORY'), // Subdiretório opcional
+        },
+      },
+    },
+  });
+  

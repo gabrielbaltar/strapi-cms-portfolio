6fbbae4036f6c4aa76531211e7ce763f9cf08eac
gabrielbaltar <201902046293@alunos.estacio.br>
2025-01-04 00:15:16 -0300

    fix: plugins.js update

--- config/plugins.ts
+++ config/plugins.ts
@@ -3,12 +3,10 @@ module.exports = ({ env }) => ({
       config: {
         provider: '@strapi/provider-upload-aws-s3',
         providerOptions: {
-          s3Options: {
-            accessKeyId: env('SUPABASE_ACCESS_KEY'),
-            secretAccessKey: env('SUPABASE_SECRET_ACCESS_KEY'),
-            region: env('SUPABASE_REGION'),
-            endpoint: env('SUPABASE_API_URL'), // URL do Supabase
-          },
+          accessKeyId: env('SUPABASE_ACCESS_KEY'), // Diretamente em providerOptions
+          secretAccessKey: env('SUPABASE_SECRET_ACCESS_KEY'),
+          region: env('SUPABASE_REGION'),
+          endpoint: env('SUPABASE_API_URL'),
           params: {
             Bucket: env('SUPABASE_BUCKET'),
           },

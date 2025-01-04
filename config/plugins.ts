6a58de231d6eed9461fe5899dbba1e873ffdeea1
gabrielbaltar <201902046293@alunos.estacio.br>
2025-01-04 00:54:13 -0300

    fix: plugins.ts error

--- config/plugins.ts
+++ config/plugins.ts
@@ -3,12 +3,14 @@ module.exports = ({ env }) => ({
       config: {
         provider: '@strapi/provider-upload-aws-s3',
         providerOptions: {
-          accessKeyId: env('SUPABASE_ACCESS_KEY'), // Diretamente em providerOptions
-          secretAccessKey: env('SUPABASE_SECRET_ACCESS_KEY'),
-          region: env('SUPABASE_REGION'),
-          endpoint: env('SUPABASE_API_URL'),
-          params: {
-            Bucket: env('SUPABASE_BUCKET'),
+          s3Options: { // Envolva as opções do S3 aqui
+            accessKeyId: env('SUPABASE_ACCESS_KEY'),
+            secretAccessKey: env('SUPABASE_SECRET_ACCESS_KEY'),
+            region: env('SUPABASE_REGION'),
+            endpoint: env('SUPABASE_API_URL'),
+            params: {
+              Bucket: env('SUPABASE_BUCKET'),
+            },
           },
         },
         actionOptions: {
